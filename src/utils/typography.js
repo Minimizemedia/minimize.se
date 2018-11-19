bf0ea7defa782d7fcf096d547a6be0c88b7008c4
Axel Ekwall <axel@minimize.se>
2018-11-19 23:04:17 +0100

    Add IBM Plex from Google Fonts

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -3,15 +3,18 @@ import Typography from 'typography';
 const typography = new Typography({
   baseFontSize: '18px',
   baseLineHeight: 1.666,
-  headerFontFamily: [
-    'Avenir Next',
-    'Helvetica Neue',
-    'Segoe UI',
-    'Helvetica',
-    'Arial',
-    'sans-serif',
-  ],
-  bodyFontFamily: ['Georgia', 'serif'],
+  headerFontFamily: ['IBM Plex Sans', 'Arial', 'sans-serif'],
+  bodyFontFamily: ['IBM Plex Serif', 'Georgia', 'serif'],
+  googleFonts: [
+    {
+      name: 'IBM Plex Sans',
+      styles: ['300', '400', '500'],
+    },
+    {
+      name: 'IBM Plex Serif',
+      styles: ['300', '400', '500'],
+    },
+  ]
 });
 
 export default typography;
