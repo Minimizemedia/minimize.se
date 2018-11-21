495b36f472978d58626cdbbb39d9cdfa2c77edc1
Axel Ekwall <axel@minimize.se>
2018-11-21 13:25:36 +0100

    Added under_construction

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import Layout from '../components/layout';
+// import Layout from '../components/layout';
 
 const NotFoundPage = () => (
-  <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that does not exist... the sadness.</p>
-  </Layout>
+  // <Layout>
+  <div style={{
+    backgroundColor: '#eddfc6',
+    width: '100vw',
+    height: '100vh',
+    display: 'flex',
+    justifyContent: 'center',
+    alignContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column'
+  }}>
+    <h1 style={{
+      color: '#00398d'
+    }}>not_found</h1>
+  </div>
+  // </Layout>
 );
 
 export default NotFoundPage;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,23 @@
 import React from 'react';
 
-import Layout from '../components/layout';
-import Image from '../components/image';
+// import Layout from '../components/layout';
+// import Image from '../components/image';
 
 const IndexPage = () => (
-  <Layout>
-    <h1>Index page</h1>
-    <p>Image:</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Image />
-    </div>
-  </Layout>
+  <div style={{
+    backgroundColor: '#eddfc6',
+    width: '100vw',
+    height: '100vh',
+    display: 'flex',
+    justifyContent: 'center',
+    alignContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column'
+  }}>
+    <h1 style={{
+      color: '#00398d'
+    }} >under_construction</h1>
+  </div>
 );
 
 export default IndexPage;
