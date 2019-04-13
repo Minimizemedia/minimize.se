6f90fc4581037425eb7755776a50852d78f41a07
Axel Ekwall <axel@minimize.se>
2019-04-13 18:54:10 +0200

    clean up

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 
-// import Layout from '../components/layout';
-// import Image from '../components/image';
-
 const IndexPage = () => (
   <div style={{
     backgroundColor: '#eddfc6',
--- src/components/header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Header = ({ siteTitle }) => (
-  <div>
-    <h1>Header, site title is: {siteTitle}</h1>
-  </div>
-);
-
-export default Header;
