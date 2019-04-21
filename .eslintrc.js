c591d2ee9a57aea01b170936a90a7f8894cedf55
Axel Ekwall <axel@minimize.se>
2019-04-21 17:45:42 +0200

    add eslint prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,3 @@
-modules.export = { extends: ['eslint:recommended', 'plugin:react/recommended'] };
+modules.export = {
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:prettier/recommended'],
+};
