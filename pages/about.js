678715bb2b74752826da59c901d603fd2052d94e
Ella Wirstad <ella.wg@hotmail.com>
2019-04-21 17:36:36 +0200

    add about + work wrappers

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import SideNav from '../components/SideNav';
+import styled from '@emotion/styled';
+
+const AboutWrapper = styled.div`
+  background-color: ${props => props.theme.colors['terra']};
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
 
 const About = () => (
-  <>
+  <AboutWrapper>
     <div>about</div>
     <SideNav
       title={'start'}
@@ -11,7 +23,7 @@ const About = () => (
       bgColor={'pale'}
       titleColor={'navy'}
     />
-  </>
+  </AboutWrapper>
 );
 
 export default About;
--- pages/work.js
+++ pages/work.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import SideNav from '../components/SideNav';
+import styled from '@emotion/styled';
+
+const WorkWrapper = styled.div`
+  background-color: ${props => props.theme.colors['navy']};
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
 
 const Work = () => (
-  <>
+  <WorkWrapper>
     <div>Work</div>
     <SideNav
       title={'start'}
@@ -11,7 +23,7 @@ const Work = () => (
       bgColor={'pale'}
       titleColor={'sage'}
     />
-  </>
+  </WorkWrapper>
 );
 
 export default Work;
