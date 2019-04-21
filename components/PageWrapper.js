47d729bb19ca5597c4e98623c1c1bd7abba5b5d1
Axel Ekwall <axel@minimize.se>
2019-04-21 19:46:45 +0200

    WIP: react spring page transition

--- components/PageWrapper.js
+++ components/PageWrapper.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from '@emotion/styled';
+import { useTransition, animated } from 'react-spring';
 
-const Wrapper = styled.div`
+const Wrapper = styled(animated.div)`
   background-color: ${props => props.theme.colors[props.bgColor]};
   width: 100vw;
   min-height: 100vh;
@@ -12,6 +13,29 @@ const Wrapper = styled.div`
   flex-direction: column;
 `;
 
-const PageWrapper = ({ bgColor, children }) => <Wrapper bgColor={bgColor}>{children}</Wrapper>;
+const PageWrapper = ({ bgColor, children }) => {
+  const [show, set] = useState(false);
+  useEffect(() => {
+    console.log('mounted');
+    set(true);
+    return () => {
+      console.log('unmounted');
+      set(false);
+    };
+  });
+  const transitions = useTransition(show, null, {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+  });
+  return transitions.map(
+    ({ item, key, props }) =>
+      item && (
+        <Wrapper key={key} style={props} bgColor={bgColor}>
+          {children}
+        </Wrapper>
+      )
+  );
+};
 
 export default PageWrapper;
